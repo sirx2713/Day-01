d76967636a90861a53ede3ace27ad49d6ae68f26
Tafadzwa Alexander Razaro <bradx2713@gmail.com>
2025-06-01 06:31:25 +0200

    Day01

---
+++ tuple.py
@@ -0,0 +1,13 @@
+#Learning Tuples in Python
+
+#Declaration of strings using sets
+me = ["Tafadzwa", "Alexander"]
+her = ["Tinotenda", "Wendy"]
+
+#Declaration of my Tuple
+my_first_tuple = (her, me)
+
+#Printing out my first Tuple in my terminal
+print(my_first_tuple)
+
+#I am also paying attention to the correct indentation
