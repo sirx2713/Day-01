db1d09815a275b635e42b767d8c836e313a2ff68
Tafadzwa Alexander Razaro <bradx2713@gmail.com>
2025-06-01 06:36:12 +0200

    Updates

--- tuple.py
+++ tuple.py
@@ -1,13 +1,21 @@
 #Learning Tuples in Python
 
 #Declaration of strings using sets
-me = ["Tafadzwa", "Alexander"]
-her = ["Tinotenda", "Wendy"]
+me = ("Tafadzwa", "Alexander")
+her = ("Tinotenda", "Wendy")
+my_age = 28
+her_age = 27
+
+#Declaration of my first dictionary
+us = {me:my_age, her:her_age}
 
 #Declaration of my Tuple
 my_first_tuple = (her, me)
 
 #Printing out my first Tuple in my terminal
 print(my_first_tuple)
+
+#Printing out my first dictionary
+print(us)
 
 #I am also paying attention to the correct indentation
