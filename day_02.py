f85fd96a05a9e0802a15ee387e5fb7d6c84fb321
Tafadzwa Alexander Razaro <bradx2713@gmail.com>
2025-06-05 16:38:38 +0200

    HeartBeat

---
+++ day_02.py
@@ -0,0 +1,5 @@
+heartbeat_numbers = input("Enter her numbers: ")
+if heartbeat_numbers == 778653649:
+    print("You know her numbers by heart!")
+elif heartbeat_numbers != 778653649:
+    print("You are such a scam!")
