1c2d73dbe50c5e3cf7215a643af56ca938377e59
Tafadzwa Alexander Razaro <bradx2713@gmail.com>
2025-06-05 16:59:46 +0200

    HeartBeat

---
+++ bitwise.py
@@ -0,0 +1,10 @@
+# Basic six bitwise operations
+# Let x = 10 (in binary0000 1010) and y = 4 (in binary0000 0100)
+x = 10
+y = 4
+print(x >> y) # Right Shift
+print(x << y) # Left Shift
+print(x & y) # Bitwise AND
+print(x | y) # Bitwise OR
+print(x ^ y) # Bitwise XOR
+print(~x) # Bitwise NOT
--- day_02.py
+++ day_02.py
@@ -1,5 +1,18 @@
 heartbeat_numbers = input("Enter her numbers: ")
+
 if heartbeat_numbers == 778653649:
     print("You know her numbers by heart!")
 elif heartbeat_numbers != 778653649:
     print("You are such a scam!")
+
+heartbeat_name = input("Enter her name: ")
+
+if heartbeat_name == "Tinotenda" or heartbeat_name == "Wendy":
+    print("You are a soulmate!")
+    print('T' in heartbeat_name)
+    print('W' in heartbeat_name)
+
+elif heartbeat_name != "Tinotenda" or heartbeat_name != "Wendy":
+    print("You are such a loser!")
+    print('T' not in heartbeat_name)
+    print('W' not in heartbeat_name)
